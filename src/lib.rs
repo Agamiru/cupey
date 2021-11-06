69b0a13ac85fb8d05f65529ea53cce1903cb9e29
Agamiru <chidinnaemekannadi@gmail.com>
2021-11-07 00:43:56 +0100

    refactored code to be void of extra overhead

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use std::env;
 use std::io::{BufRead, Read, Write, BufReader, Lines};
 
 use clap::{Arg, SubCommand, App};
+use walkdir::WalkDir;
 
 mod errors;
 mod cupey_traits;
@@ -20,10 +21,9 @@ type LinesFromFile = Lines<BufReader<fs::File>>;
 
 #[derive(Debug)]
 pub struct Cupey<'a> {
-    origin: Option<String>,
+    // origin: String,
     current_dir: PathBuf,
     matches: clap::ArgMatches<'a>,
-    settings: Settings
     // roll_back: Option<String>,
 }
 
@@ -44,167 +44,100 @@ impl<'a> Cupey<'a> {
             .about("Recursively copy files from one folder to another")
             .author("Chidi Nnadi");
 
-        let setup_command = SubCommand::with_name("set-origin")
-            .arg(
-                Arg::with_name("originating_dir")
-                    .help("The folder to be copied from")
-                    .index(1)
-                    .value_name("PATH")
-                    .required(true)
-                    .validator(|value| {
-                        let path = Path::new(&value);
-                        if path.exists() {
-                            if path.is_dir() {
-                                return Ok(());
-                            }
-                            return Err("Sorry this path isn't a directory".to_owned());
-                        } else {
-                            return Err("Sorry this path doesnt't exist".to_owned());
-                        }
-                    })                
-                );
-
-        let app = app.subcommand(setup_command);
-
-        let orig_dir_option = Arg::with_name("from-origin")
-            .long("from-origin")
-            .short("fo")
+        let from_arg = Arg::with_name("from_arg")
+            .help("The folder location to copy from")
+            .short("f")
+            .long("from")
             .value_name("PATH")
-            .help("Path to the folder you want to 'cupey' from");
+            .takes_value(true)
+            .validator(|value| {
+                let path = Path::new(&value);
+                if path.exists() {
+                    if path.is_dir() {
+                        return Ok(());
+                    }
+                    return Err("Sorry this path isn't a directory".to_owned());
+                } else {
+                    return Err("Sorry this path doesnt't exist".to_owned());
+                }
+            });
+
+        let app = app.arg(from_arg);
 
         let matches = app.get_matches_from_safe(args)?;
-        let settings = Settings::new();
-        let origin_as_string: Option<String>;
-        if settings.reader.is_some() {
-            origin_as_string = Some(settings.origin_as_string()?);
-        } else {
-            origin_as_string = None;
-        }
 
         return Ok(Cupey {
-            origin: origin_as_string, current_dir, matches, settings
+            current_dir, matches
         });
-        
-
-    //     if let Some(arg_matches) = matches.subcommand_matches("set-origin") {
-    //         let origin = arg_matches.value_of("originating_dir").unwrap().to_owned();
-    //         let dest_file_path = Path::new(CUPEY_SETTINGS_FILE);
-    //         write_from_string(&format!("{}\n", origin), dest_file_path).unwrap();
-
-    //         return Ok(Cupey {origin, current_dir});
-            
-    //     } else if matches.is_present("from-origin") {
-    //         let origin = ReadFromSettings::new()
-    //             .origin_as_string()
-    //             .map_err(|err| clap::Error {
-    //                 message: format!("Couldn't get origin: {}", err.to_string()),
-    //                 kind: clap::ErrorKind::InvalidValue, info: None
-    //             })?;
-    //             return Ok(Cupey {origin, current_dir});
-            
-    //     } else {
-    //         return Err(clap::Error{
-    //             message: "Improperly Configured".to_owned(),
-    //             kind: clap::ErrorKind::ArgumentNotFound,
-    //             info: None
-    //         })
-    //     }
     }
-
-
-    fn set_origin(&mut self) {
-        if let Some(arg_matches) = self.matches.subcommand_matches("set-origin") {
-            let origin = arg_matches.value_of("originating_dir").unwrap().to_owned();
-            let p = Settings::settings_file_path();
-            let dest_file_path = Path::new(&p);
-            self.settings.perform_write(
-                dest_file_path, format!("{}\n", origin), Some("txt")
-            );
-            self.origin = Some(origin);
-        }
-    }
-
     
-    fn copy_files(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let path = self.origin.clone().unwrap();
-        visit_dirs(&Path::new(&path), &self.current_dir)?;
+    pub fn copy_files(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let originating_dir = self.matches.value_of("from_arg").unwrap();
+        visit_dirs(&Path::new(originating_dir), &self.current_dir)?;
         Ok(())
     }
 }
 
+fn visit_dirs(dir: &Path, to_dir: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
 
-// impl<'a> cupey_traits::ResourceManager for Cupey<'a> {}
+    // let mut perms = fs::metadata(dir)?.permissions();
+    // perms.set_readonly(true);
+    // fs::set_permissions(dir, perms)?;
+    // println!("I ran");
 
+    if dir.is_file() || to_dir.is_file() {
+        println!("Neither of these should be files!");
+        return Ok(());
+    }
 
-#[derive(Debug)]
-pub struct Settings {
-    reader: Option<LinesFromFile>
-}
+    // if fs::read_dir(dir)?.next().is_none() {
+    //     println!("Folder is empty\n");
+    //     return Ok(());
+    // }
+    // Check if folder is empty
+    if fs::read_dir(dir).into_iter().next().is_none() {
+    // if WalkDir::new(dir).into_iter().next().is_none() {
+        println!("Folder is empty\n");
+        return Ok(());
+    }
 
-impl  Settings {
+    println!("I ran");
 
-    fn new() -> Self {
-        if Self::file_exists() {
-            Self::from_file()
+    // Recurse through folder.
+    for entry in fs::read_dir(dir)? {
+    // for entry in WalkDir::new(dir) {
+        let entry = entry?;
+        let entry_path = entry.path();
+
+        println!("entry_path: {:?}", entry_path );
+
+        // block for handling folders
+        if entry_path.is_dir() {
+            println!("Path is folder");
+            println!("Folder path: {:?}", entry_path);
+            if let Some(folder_name) = entry_path.file_name() {
+                // Create a new directory in destination path
+                let mut new_dest_dir = to_dir.to_owned();
+                new_dest_dir.push(folder_name);
+
+                if !new_dest_dir.exists() {
+                    fs::create_dir(&new_dest_dir)?;
+                }
+                visit_dirs(&entry_path, &new_dest_dir)?;
+            }
+        
+        // block for handling files     
         } else {
-            Self {reader: None}
+            println!("Copying file {:?}", &entry_path);
+
+            let mut new_dest_dir = to_dir.to_owned();
+            copier(&entry_path.to_owned(), &mut new_dest_dir)?;
         }
     }
 
-    pub fn from_file() -> Self {
-        let reader = Self::settings_file_lines()
-            .map_err(|err| panic!(
-                    "Failed to create Cupey object for this reason:\n'{}'",
-                    err.to_string()
-                )
-            );
-        Settings{ reader: Some(reader.unwrap()) }
-    }
-
-    pub fn settings_file_lines() -> Result<LinesFromFile, std::io::Error> {
-        let settings_file = Self::settings_file()?;
-        let settings_reader = BufReader::new(settings_file);
-        Ok(settings_reader.lines())
-    }
-
-    // fn origin_file_path() -> &'static Path {
-    //     return Path::new(Self::settings_file_path());
-    // }
-
-    fn settings_file() -> Result<fs::File, std::io::Error> {
-        let settings_file = fs::OpenOptions::new()
-            .read(true)
-            .open(Path::new(&Self::settings_file_path()))?;
-        
-        Ok(settings_file)
-    }
-
-    fn origin_as_string(&self) -> Result<String, std::io::Error> {
-        let mut reader = self.clone().reader
-            .unwrap();
-        Ok((reader.next().unwrap()?).to_owned())
-    }
-
-    fn settings_file_path() -> String {
-        let path = format!("{}\\cupey_settings.txt", CUPEY_HOME_DIR);
-        path
-    }
-
-    fn file_exists() -> bool {
-        Path::new(&Self::settings_file_path()).exists()
-    }
+    Ok(())
 
 }
-
-impl Clone for Settings {
-    fn clone(&self) -> Self {
-        Self::from_file()
-    }
-}
-
-impl cupey_traits::ResourceManager for Settings {}
-
-
 
 fn write_from_string(string: &str, dest_file_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     // Check if file is a text file
@@ -234,48 +167,6 @@ fn get_extension_from_filename(filename: &str) -> Option<&str> {
         .and_then(std::ffi::OsStr::to_str)
 }
 
-
-fn visit_dirs(dir: &Path, to_dir: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
-
-    if dir.is_file() || to_dir.is_file() {
-        println!("Neither of these should be files!");
-        return Ok(());
-    }
-
-    if dir.read_dir()?.next().is_none() {
-        println!("Folder is empty\n");
-        return Ok(());
-    }
-
-    // Recurse through folder.
-    for entry in fs::read_dir(dir)? {
-        let entry = entry?;
-        let path = entry.path();
-        if path.is_dir() {
-            println!("Folder path: {:?}", path);
-            if let Some(folder_name) = path.file_name() {
-                // Create a new directory in destination path
-                let mut new_dest_dir = to_dir.to_owned();
-                new_dest_dir.push(folder_name);
-
-                if !new_dest_dir.exists() {
-                    fs::create_dir(&new_dest_dir)?;
-                }
-                visit_dirs(&path, &new_dest_dir)?;
-            }
-        
-        // path is a file     
-        } else {
-            println!("Copying file {:?}", &path);
-
-            let mut new_dest_dir = to_dir.to_owned();
-            copier(&path, &mut new_dest_dir)?;
-        }
-    }
-
-    Ok(())
-
-}
 
 fn copier(orig_file_path: &PathBuf, destination_dir: &mut PathBuf) -> Result<(), Box<dyn std::error::Error>> {
     // Get file name to append to new destination path
@@ -316,21 +207,38 @@ fn copier(orig_file_path: &PathBuf, destination_dir: &mut PathBuf) -> Result<(),
 mod tests {
     use super::*;
 
-
     // #[test]
-    fn test_improperly_configured() {
-        let cupey_inst = Cupey::new_from(["exename"].iter());
-        assert!(cupey_inst.is_err());
-        assert_eq!(&cupey_inst.unwrap_err().message, "Improperly Configured");
-    }
+    // fn test_improperly_configured() {
+    //     let cupey_inst = Cupey::new_from(["exename"].iter());
+    //     assert!(cupey_inst.is_err());
+    //     assert_eq!(&cupey_inst.unwrap_err().message, "Improperly Configured");
+    // }
 
     // #[test]
     fn test_properly_configured() {
-        let cupey_inst = Cupey::new_from([
-            "exename", "set-origin", "C:\\Users\\hp\\Desktop\\test_folder"
+        let mut cupey_inst = Cupey::new_from([
+            "cupey", "--from", "/Users/mac/Desktop/cupey_from"
             ]
             .iter()).unwrap();
-        assert!(cupey_inst.copy_files().is_ok());
+        let mut current_dir = PathBuf::new();
+        current_dir.push(Path::new("/Users/mac/Desktop/cupey_into"));
+        cupey_inst.current_dir = current_dir;
+        let err  = cupey_inst.copy_files().unwrap();
+        println!("{:?}", err);
+        // assert!(cupey_inst.copy_files().is_ok());
+    }
+
+    #[test]
+    fn test_walkdir() {
+        let copy_from = Path::new("/Users/mac/Desktop/cupey_from");
+        // for dir_entry in WalkDir::new(copy_from) {
+        for dir_entry in copy_from.read_dir().unwrap() {
+            println!(" I ran");
+            // let entry = dir_entry.unwrap();
+            println!("{:?}", dir_entry)
+        }
+        // println!("{:?}", fs::read_dir(copy_from).unwrap())
+
     }
 
     // #[test]
@@ -373,7 +281,7 @@ mod tests {
     impl ResourceManager for TestStruct {}
 
     
-    #[test]
+    // #[test]
     fn test_resource_manager() {
         let file_path = Path::new("C:\\Users\\hp\\Desktop\\Cupey");
         let test_struct = TestStruct{};
